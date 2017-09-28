6043556bd2bd68daa674caba36d51e8c482a5570
Ilaf-kaladiya <shahid.the.coder@gmial.com>
2017-09-28 11:59:05 +0530

    DONE

---
+++ quicksort.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#define MAX 100
+
+
+
+int split(int arr[],int lower,int upper)
+{
+	int pivot = lower,temp;
+	int a = pivot+1,b = upper;
+	
+	while(b>=a)
+	{
+		while(arr[a] < arr[pivot])
+			a++;
+		while(arr[b] > arr[pivot])
+			b--;
+			
+		if(b>a)
+		{
+			temp = arr[a];
+			arr[a] = arr[b];
+			arr[b] = temp;
+		}
+	}
+	temp = arr[b];
+	arr[b] = arr[pivot];
+	arr[pivot]=temp;
+	
+
+	return b;
+}
+
+void Quick_sort(int arr[],int lower,int upper)
+{
+	int i;
+	if(upper>lower){
+	i = split(arr,lower,upper);
+	Quick_sort(arr,lower,i-1);
+	Quick_sort(arr,i+1,upper);
+	}
+}
+
+int main()
+{ 
+	int a[MAX],n,i;
+	
+	printf("\nEnter the number of elements for array.\n");
+	scanf("%d",&n);
+	if(n>100)
+	printf("SORRY.\nNumber of elements should be less than 100\n");
+	else {
+	printf("Enter elements of array\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	
+	
+	Quick_sort(a,0,n-1);
+	for(i=0;i<n;i++){
+		printf("%d\t",a[i]);
+		}
+	}	
+	return 0;
+}
