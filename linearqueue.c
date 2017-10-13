a6aa816756b8941b342757f3387cc3124fa72f32
Ilaf-kaladiya <kaladiyailaf@gmail.com>
2017-10-13 13:49:27 +0000

    Update linearqueue.c

--- linearqueue.c
+++ linearqueue.c
@@ -1,5 +1,12 @@
-#include<stdio.h>
+/*Description: Array implementation of Queue
+ * Learner: ILAF KALADIYA
+ * created on: 10th AUGUST 2017
+ */
 
+
+
+#include<stdio.h>
+#include<stdlib.h>
 #define MAXSIZE 10
 
 
@@ -29,7 +36,7 @@ int peek(){
 }
 
 void insertQueue(int e)
-{
+{ 					//to insert an element in a Queue
 	if(!isFull())
 	{
 		if(isEmpty())
@@ -37,17 +44,17 @@ void insertQueue(int e)
 			front++;
 		}
 		queue[++rear]=e;
-		printf("your element is successfully inserted\n");
+		printf("Your element is successfully inserted\n");
 	}
 	else
 	{
-		printf("Queue is full \n element cannot be inserted");
+		printf("Queue is full \n Element cannot be inserted\n");
 	}
 	
 	
 	}
 void deleteQueue()
-{
+{					//to delete an element from a Queue
 	if(!isEmpty())
 	{
 		int d=queue[front];
@@ -59,7 +66,7 @@ void deleteQueue()
 		{
 			front++;
 		}
-		printf("element deleted id %d\n",d);
+		printf("Element deleted is %d\n",d);
 	}
 	}
 
@@ -69,23 +76,27 @@ int main(){
 	int choice,e;
 	do 
 	{
-		printf("display menu\n1.peek\n2.insert\n3.delete\n4.exit\n");
-		printf("enter choice");
+		printf("\nDisplay Menu\n1.Peek\n2.Insert\n3.Delete\n4.Exit\n");
+		printf("Enter choice\n");
 		scanf("%d",&choice);
 		switch(choice){
 			case 1:
 			
-			printf("element at frontof queue  %d",peek());
+			printf("Element at frontof queue : %d",peek());
 			
 				break;
 			case 2:
-			printf("enter element to be inserted ");
+			printf("Enter element to be inserted ");
 			scanf("%d",&e);
 			insertQueue(e);
 				break;
 			case 3:deleteQueue();
 			break;
-			case 4:return 0;
+			case 4: 
+			exit(0);
+			break;
+			default :
+			printf("Invalid choice\n");
 		} 
 		
 	} while(1);
@@ -94,3 +105,92 @@ int main(){
 		
 
 
+/*
+Display Menu
+1.Peek
+2.Insert
+3.Delete
+4.Exit
+Enter choice
+2
+Enter element to be inserted 10
+Your element is successfully inserted
+
+Display Menu
+1.Peek
+2.Insert
+3.Delete
+4.Exit
+Enter choice
+2
+Enter element to be inserted 20
+Your element is successfully inserted
+
+Display Menu
+1.Peek
+2.Insert
+3.Delete
+4.Exit
+Enter choice
+2
+Enter element to be inserted 30
+Your element is successfully inserted
+
+Display Menu
+1.Peek
+2.Insert
+3.Delete
+4.Exit
+Enter choice
+2
+Enter element to be inserted 40
+Your element is successfully inserted
+
+Display Menu
+1.Peek
+2.Insert
+3.Delete
+4.Exit
+Enter choice
+1
+Element at frontof queue : 10
+Display Menu
+1.Peek
+2.Insert
+3.Delete
+4.Exit
+Enter choice
+3
+Element deleted is 10
+
+Display Menu
+1.Peek
+2.Insert
+3.Delete
+4.Exit
+Enter choice
+1
+Element at frontof queue : 20
+Display Menu
+1.Peek
+2.Insert
+3.Delete
+4.Exit
+Enter choice
+5
+Invalid choice
+
+Display Menu
+1.Peek
+2.Insert
+3.Delete
+4.Exit
+Enter choice
+4
+
+
+------------------
+(program exited with code: 0)
+Press return to continue
+
+*/
